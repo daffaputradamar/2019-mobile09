9ac8318714445c4c56584d7fd21c164bd3c0fcfb
Daffa Akbar <daffaputradamar@gmail.com>
2019-10-25 16:50:42 +0700

    tugas 2 selesai

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
@@ -26,10 +26,22 @@ public class MainActivity extends AppCompatActivity {
         listSuperHero.add(hero);
         hero = new SuperHero("Gareng", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMEkvUp9OwvFV0KTjIfAxGMqVrD4ACV3jvVTOrKDE3HNYyH5X6");
         listSuperHero.add(hero);
+        hero = new SuperHero("Gatotkaca", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTc97V1QtHsBD6X103rRuQQJBYlf2QL-KoxJh7ypWEYNFTzP9HC");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Werkudara", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgYSJm1FAQ08i_pnOHMKIDMSf1zFT8WvwfA-aS1SlFDNIKzu");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Arjuna", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRSj2RfkC9-NpUGe7SoWZpjAqWrk1nbHYuzpY2T_sORFYZ3yET8");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Bima", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMEkvUp9OwvFV0KTjIfAxGMqVrD4ACV3jvVTOrKDE3HNYyH5X6");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Nakula", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQL3s6Jap-LJdTH5OtE5plhXmu8JfOFPt8Ilq9JL4c7FJ-jiXWW");
+        listSuperHero.add(hero);
+        hero = new SuperHero("Sadewa", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMEkvUp9OwvFV0KTjIfAxGMqVrD4ACV3jvVTOrKDE3HNYyH5X6");
+        listSuperHero.add(hero);
 
         SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
 
         rvSuperHero.setAdapter(superHeroAdapter);
-        rvSuperHero.setLayoutManager(new LinearLayoutManager(this));
+        rvSuperHero.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 }
